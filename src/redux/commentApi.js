30152a0b2b2e3d0f855482cf2a6f40a1f13837dd
Olena Shatalova <08082023m@gmail.com>
2024-11-17 11:15:16 +0200

    add mockApi

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,15 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = '';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://671f3bcae7a5792f052d71c7.mockapi.io";
 
 export const commentApi = createApi({
-  reducerPath: 'comments',
+  reducerPath: "comments",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ['Comments'],
-  endpoints: (builder) => ({}),
+  tagTypes: ["Comments"],
+  endpoints: (builder) => ({
+    getComments: builder.query({
+      query: () => ({ url: API_ENDPOINT }),
+    }),
+  }),
 });
 
-export const {} = commentApi;
+export const { useGetCommentsQuery } = commentApi;
